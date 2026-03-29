9a0d2c310b3caee530f1894202e45d3e8caea0ee
Aseem <aseemjan7@gmail.com>
2026-03-29 21:25:30 +0530

    feat: add authentication endpoint with JWT response and login request DTOs

--- src/main/java/com/aseem/lakesidehotel/controller/AuthController.java
+++ src/main/java/com/aseem/lakesidehotel/controller/AuthController.java
@@ -4,18 +4,34 @@ import com.aseem.lakesidehotel.exception.UserAlreadyExistsException;
 
 import com.aseem.lakesidehotel.model.User;
 
+import com.aseem.lakesidehotel.request.LoginRequest;
+import com.aseem.lakesidehotel.response.JwtResponse;
+import com.aseem.lakesidehotel.security.jwt.JwtUtils;
+import com.aseem.lakesidehotel.security.user.HotelUserDetails;
 import com.aseem.lakesidehotel.service.IUserService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 public class AuthController {
     private final IUserService userService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtils jwtUtils;
 
     @PostMapping("/register-user")
     public ResponseEntity<?> registerUser(User user){
@@ -25,5 +41,23 @@ public class AuthController {
         }catch(UserAlreadyExistsException e){
             return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
         }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest request){
+        Authentication authentication =
+                authenticationManager
+                        .authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtTokenForUser(authentication);
+        HotelUserDetails userDetails = (HotelUserDetails) authentication.getPrincipal();
+        List<String> roles = userDetails.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority).toList();
+        return ResponseEntity.ok(new JwtResponse(
+                userDetails.getId(),
+                userDetails.getEmail(),
+                jwt,
+                roles));
     }
 }
